b6468739486f09bd7a80f568dacc1812f365ec71
Kwantz <erickkwantantz123@gmail.com>
2019-04-21 15:31:26 +0700

    Update template

--- template/2019-04-21.cpp
+++ template/2019-04-21.cpp
@@ -10,6 +10,9 @@ typedef vector<ii> vii;
 #define INF 1e9
 #define EPS 1e-9
 
+#define pb push_back
+#define mp make_pair
+
 #define initDP(arr) memset(arr, -1, sizeof arr)
 #define clearArr(arr) memset(arr, 0, sizeof arr)
 
